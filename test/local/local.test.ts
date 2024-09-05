f8c1217bbbf0f4c6abe25a572041cba24ee9fbf8
Nicolas Merget <nicolas.merget@deutschebahn.com>
2024-09-05 08:11:51 +0200

    chore: replace jest with vitest

--- test/local/local.test.ts
+++ test/local/local.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, test } from "@jest/globals";
+import { describe, expect, test } from "vitest";
 import generateIconFonts from "../../src/generate-icon-fonts";
 import fs from "fs";
 
